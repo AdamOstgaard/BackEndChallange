7758012f06ac523a4fcef9738c07aaeb9c431470
Adam Östgaard <Adam@ostgaard.nu>
2018-01-29 20:15:54 +0100

    All features are workingerror handling for input needs to be done. Have to redo the way city list works as loading 500mb to ram is not suitable...

---
+++ WeatherApp/WeatherApp/CityManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    class CityManager
+    {
+        private JArray _Cities = new JArray();
+        public CityManager(string CityListLocation)
+        {
+            string cities = File.ReadAllText(CityListLocation);
+            _Cities = JArray.Parse(cities);
+        }
+
+        public string GetIdFromName(string name) {
+            var cities = _Cities.Children().ToList();
+            foreach(JObject t in cities)
+            {
+                if (t["name"].ToString().ToLower().Equals(name.ToLower()))
+                    return t["id"].ToString();
+            }
+            return null;
+        }
+    }
+}
--- WeatherApp/WeatherApp/Program.cs
+++ WeatherApp/WeatherApp/Program.cs
@@ -8,10 +8,14 @@ namespace WeatherApp
 {
     class Program
     {
+        private const string _APIKEY = "b21da8b713fbce72cdc1d0d012875d12";
+        private const string _CITYLISTLOCATION = "Resources\\city.list.json";
         static void Main(string[] args)
         {
-
-            DataGrabber dataGrabber = new DataGrabber("2673730", "b21da8b713fbce72cdc1d0d012875d12");
+            string input = Console.ReadLine();
+            CityManager c = new CityManager(_CITYLISTLOCATION);
+            //TODO: Add exceptionhandling for when location is not valid and for when city list is non existing.
+            DataGrabber dataGrabber = new DataGrabber(c.GetIdFromName(input), _APIKEY);
             var t = dataGrabber.getWeather();
             Console.WriteLine(t.ToString());
             Console.ReadKey();
