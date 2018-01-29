eccf5d12892f1c54f77c60fa2c2cf87afb339315
Adam Östgaard <Adam@ostgaard.nu>
2018-01-29 15:28:54 +0100

    Added basic Uri creation with OpenWeatherMap as content provider, added json file with name definitions for cities.

---
+++ WeatherApp/WeatherApp/DataGrabber.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * This class uses API from open weathermap to get current weather.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class DataGrabber
+    {
+        private string _ApiKey;
+        private string _CityId;
+        private Uri _ApiRequest { get
+            {
+                return new Uri(string.Format("api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", _CityId, _ApiKey));
+            } }
+        /// <summary>
+        /// Creates a new DataGrabber object containing the 
+        /// </summary>
+        /// <param name="DataSource">The API command to execute</param>
+        /// <exception cref="ArgumentNullException">Gets thrown if a parameter is null</exception>
+        public DataGrabber(string CityId, string ApiKey)
+        {
+            if (!string.IsNullOrWhiteSpace(CityId))
+                _CityId = CityId;
+            else
+                throw new ArgumentNullException("CityId");
+            if (!string.IsNullOrWhiteSpace(ApiKey))
+                _ApiKey = ApiKey;
+            else
+                throw new ArgumentNullException("ApiKey");
+        }
+        /// <summary>
+        /// Updates the weather object
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+}
